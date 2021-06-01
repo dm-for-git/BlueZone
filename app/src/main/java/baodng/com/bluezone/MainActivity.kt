e8a38ae6f24e60543e04dcc14691e998a061abfa
dm-for-git <dm-for-git@protonmail.com>
2021-06-01 15:10:00 +0700

    Init project

---
+++ app/src/main/java/baodng/com/bluezone/MainActivity.kt
@@ -0,0 +1,14 @@
+package baodng.com.bluezone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val x = 1/0
+        Toast.makeText(this, "$x", Toast.LENGTH_SHORT).show()
+    }
+}
